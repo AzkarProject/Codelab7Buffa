5a4f1baca2380d52ee16916d3856a9608015d7cb
azkarproject <azkartest@gmail.com>
2015-03-06 11:37:12 +0100

    ...

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
+
+/*
 var static = require('node-static');
 var http = require('http');
 var file = new(static.Server)();
 // Titi :  Factorisation du numéro de port
 // Teste si présence d'une variable d'environnement Hosting (Heroku) sinon port 2013 pour local 
 var port = process.env.PORT || 2013;
+
 var app = http.createServer(function (req, res) {
   file.serve(req, res);
 }).listen(port);
@@ -40,6 +43,8 @@ console.log('listen on port'+port);
 var io = require('socket.io').listen(app);
 // Titi - Contrôle de la version de socket.io
 console.log("**Socket.IO Version: " + require('socket.io/package').version);
+
+
 io.sockets.on('connection', function (socket){
 
 	// Permet d'envoyer des traces au client distant
@@ -78,4 +83,96 @@ io.sockets.on('connection', function (socket){
 	});
 
 });
+/**/
 
+// ------------------------ Version 2
+
+var static = require('node-static');
+var http = require('http');
+var file = new(static.Server)();
+var port = process.env.PORT || 2013;
+var app = http.createServer(function (req, res) {
+  file.serve(req, res);
+}).listen(port);
+console.log('listen on port'+port);
+var io = require('socket.io').listen(app);
+io.sockets.on('connection', function (socket){
+	function log(){
+		var array = [">>> "];
+	  for (var i = 0; i < arguments.length; i++) {
+	  	array.push(arguments[i]);
+	  }
+	    socket.emit('log', array);
+	}
+	socket.on('message', function (message) {
+		log('Got message: ', message);
+		socket.broadcast.emit('message', message); // should be room only
+	});
+	socket.on('create or join', function (room) {
+		var numClients = io.sockets.clients(room).length;
+		log('Room ' + room + ' has ' + numClients + ' client(s)');
+		log('Request to create or join room', room);
+		if (numClients == 0){
+			socket.join(room);
+			socket.emit('created', room);
+		} else if (numClients == 1) {
+			io.sockets.in(room).emit('join', room);
+			socket.join(room);
+			socket.emit('joined', room);
+		} else { // max two clients
+			socket.emit('full', room);
+		}
+	});
+});
+
+/**/
+
+
+
+/*
+var app = require('express')(),
+    server = require('http').createServer(app),
+    io = require('socket.io').listen(server);
+
+ // Add pour 1-openshift 2-heroku 3-local
+var ipaddress = process.env.OPENSHIFT_NODEJS_IP || process.env.IP || "127.0.0.1" ;
+var port      = process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 2013;
+app.set('port', port); // Pour Openshift
+
+// Chargement de la page index.html
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/index.html');
+});
+
+
+io.sockets.on('connection', function (socket){
+	function log(){
+		var array = [">>> "];
+	  for (var i = 0; i < arguments.length; i++) {
+	  	array.push(arguments[i]);
+	  }
+	    socket.emit('log', array);
+	}
+	socket.on('message', function (message) {
+		log('Got message: ', message);
+		socket.broadcast.emit('message', message); // should be room only
+	});
+	socket.on('create or join', function (room) {
+		var numClients = io.sockets.clients(room).length;
+		log('Room ' + room + ' has ' + numClients + ' client(s)');
+		log('Request to create or join room', room);
+		if (numClients == 0){
+			socket.join(room);
+			socket.emit('created', room);
+		} else if (numClients == 1) {
+			io.sockets.in(room).emit('join', room);
+			socket.join(room);
+			socket.emit('joined', room);
+		} else { // max two clients
+			socket.emit('full', room);
+		}
+	});
+});
+
+server.listen(app.get('port'),ipaddress);
+/***/
